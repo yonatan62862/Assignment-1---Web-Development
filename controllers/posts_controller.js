8d70829014b1c02fb2a13269af5ecf314f29c906
Yonatan <yonatan62862@gmail.com>
2024-11-16 17:12:14 +0200

    getPostsBySender+updatePost

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -9,6 +9,25 @@ const getAllPosts = async (req, res) => {
   }
 };
 
+const getPostsBySender = async (req, res) => {
+  try {
+    const sender = req.query.sender; 
+    if (!sender) {
+      return res.status(400).json({ error: "Missing sender parameter" });
+    }
+
+    const posts = await Posts.find({ sender: sender });
+    if (posts.length === 0) {
+      return res.status(404).json({ message: "No posts found for this sender" });
+    }
+
+    return res.status(200).json(posts); 
+  } catch (err) {
+    res.status(500).json({ error: err.message }); 
+  }
+};
+
+
 const getPostById = async (req,res) => {
   const id = req.params.id;
   if(id) {
@@ -36,9 +55,24 @@ const createPost = async (req, res) => {
   }
 };
 
-const deletePost = (req, res) => {
-  console.log("delete a post");
-  res.send("delete a post");
-};
 
-module.exports = { getAllPosts, createPost, deletePost, getPostById };
+const updatePost = async (req,res) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+    try {
+      const updatedPost = await Posts.findByIdAndUpdate(id, updatedData, {
+        new: true,
+        runValidators: true,
+      });
+
+      if (!updatedPost) {
+        return res.status(404).json({ message: "Post not found" });
+      } else {
+         return res.status(200).json(updatedPost);
+      }
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+module.exports = { getAllPosts, createPost, getPostById, getPostsBySender, updatePost };
--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts_controller");
 
-router.get("/", postsController.getAllPosts);
+router.get("/by-sender", postsController.getPostsBySender);
 
 router.get("/:id", postsController.getPostById);
 
+router.get("/", postsController.getAllPosts);
+
 router.post("/",postsController.createPost);
 
-router.delete("/:id",postsController.deletePost);
+router.put("/:id", postsController.updatePost);
+
+
 
 module.exports = router;
